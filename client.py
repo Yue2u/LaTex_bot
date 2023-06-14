8dae73ccaf56ba602b3141ccbc11dabf14e6bc71
Yue2u <beloleg225@mail.ru>
2023-06-15 01:04:53 +0300

    Adde Flask server

--- client.py
+++ client.py
@@ -18,9 +18,9 @@ async def recognize(images_path, url):
         encoded_images[idx] = encoded
 
     data = json.dumps(encoded_images, indent=4, ensure_ascii=False)
-
     async with client.post(url + '/process', data=data, headers={"ngrok-skip-browser-warning": "1"}) as resp:
         if resp.status != 200:
             raise ValueError("Something went wrong. Try later")
         text = await resp.text()
+        print(text)
         return json.loads(text)
